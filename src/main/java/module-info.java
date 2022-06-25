9ec437b3ba48f5e224be17f34d5591238bf63adf
alejandro <dsepulvedaa@unal.edu.co>
2022-06-25 10:08:29 -0500

    v.1.1  se agregan los primeros archivos de la database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module Sistema_administrativo_de_tienda.controlador {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens Sistema_administrativo_de_tienda.controlador to javafx.fxml;
     exports Sistema_administrativo_de_tienda.controlador;
