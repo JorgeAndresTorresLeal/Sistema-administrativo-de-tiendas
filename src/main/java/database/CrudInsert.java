3c332a6747566f91474e87e746b025204f297140
alejandro <dsepulvedaa@unal.edu.co>
2022-06-28 02:36:40 -0500

    actualizando el insert

--- src/main/java/database/CrudInsert.java
+++ src/main/java/database/CrudInsert.java
@@ -11,17 +11,18 @@ public class CrudInsert {
         this.conexionDb = conexionDb;
     }
 
-    public void insertarPr(String name, int cantidad, String fechaVencimiento, float precio){
+    public void insertarPr(String name, float cantidad, String fechaVencimiento, float preciov, float precioc){
         String sql = "INSERT INTO public.productos(\n" +
-                "\t name, cantidad, precio, vencimiento)\n" +
-                "\tVALUES ( ?, ?, ?, ?);";
+                "\t name, cantidad, precio, vencimiento, precioc)\n" +
+                "\tVALUES ( ?, ?, ?, ?, ?);";
 
         try{
             PreparedStatement statement = this.conexionDb.prepareStatement(sql);
             statement.setString(1,name);
-            statement.setInt(2,cantidad);
-            statement.setFloat(3,precio);
+            statement.setFloat(2,cantidad);
+            statement.setFloat(3,preciov);
             statement.setDate(4,Date.valueOf(fechaVencimiento));
+            statement.setFloat(5, precioc);
             int rowInsertd = statement.executeUpdate();
             System.out.println((rowInsertd>0)?"inserccion exitosa!":"inserccion fallida!");
         }catch (SQLException e){
@@ -29,16 +30,17 @@ public class CrudInsert {
         }
     }
 
-    public void insertarUs(int documento, String user, String pass, String name){
+    public void insertarUs(int documento, String user, String pass, String name, boolean bool){
         try {
             String sql ="INSERT INTO public.usuarios(\n" +
-                    "\tid, usuario, password, name)\n" +
-                    "\tVALUES (?, ?, ?, ?);";
+                    "\tid, usuario, password, name, jefe)\n" +
+                    "\tVALUES (?, ?, ?, ?, ?);";
             PreparedStatement statement = conexionDb.prepareStatement(sql);
             statement.setInt(1,documento);
             statement.setString(2,user);
             statement.setString(3,pass);
             statement.setString(4,name);
+            statement.setBoolean(5, bool);
             int rowInserted=statement.executeUpdate();
             System.out.println((rowInserted>0)?"inserccion exitosa!":"inserccion fallida!");
         }catch (SQLException e){
